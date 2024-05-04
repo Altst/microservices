e33d12882b8915a5427c0c809d4fc9c437b793c1
Altst <94057001+Altst@users.noreply.github.com>
2024-05-04 20:16:30 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyGnLhhLLeHN_eWqjIkXyYf-8Ig6dHLgQ",
-  authDomain: "notes-766d1.firebaseapp.com",
-  projectId: "notes-766d1",
-  storageBucket: "notes-766d1.appspot.com",
-  messagingSenderId: "457812775339",
-  appId: "1:457812775339:web:16d4e26b46d13baa5942ee"
+  apiKey: "AIzaSyCZUG3Im8xY0KXvAFtkedaX0xBuwm18NGw",
+  authDomain: "microservices-5748f.firebaseapp.com",
+  projectId: "microservices-5748f",
+  storageBucket: "microservices-5748f.appspot.com",
+  messagingSenderId: "613825179530",
+  appId: "1:613825179530:web:b79befb5beee742e73a273"
 };
 
 // Initialize Firebase
